811cf9f3a9b5443b42335cb4918d0b06c4217bc1
Nataliya Rohochaya <lc4821577@gmail.com>
2020-12-21 11:05:05 +0200

    feature: added section viewed products

--- wp-content/themes/mebli/assets/js/main.js
+++ wp-content/themes/mebli/assets/js/main.js
@@ -42,4 +42,25 @@ document.addEventListener('DOMContentLoaded', function () {
         //     prevEl: '.swiper-button-prev',
         // },
     });
+
+    let mySwiperProducts = new Swiper('.swiper-products', {
+        // Optional parameters
+        slidesPerView: 1,
+        // preventClicks: false,
+        breakpoints: {
+            650: {
+                slidesPerView: 2,
+                spaceBetween: 40,
+            },
+            993: {
+                slidesPerView: 3,
+                spaceBetween: 60,
+            }
+        },
+        // Navigation arrows
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+    });
 });
--- wp-content/themes/mebli/pages/home.php
+++ wp-content/themes/mebli/pages/home.php
@@ -84,4 +84,51 @@
                 </div>
             </div>
         </section>
+    <section class='viewed-products'>
+        <div class='container'>
+            <!-- Slider main container-->
+            <div class="swiper-container swiper-products">
+                <!--Additional required wrapper-->
+                <div class="swiper-wrapper">
+                    <!-- Slides -->
+                    <div class="swiper-slide">
+                        <div class='viewed-products-item'>
+                            <div class='item-image'>
+                                <img src='<?php echo get_template_directory_uri() ?>/assets/images/catalog-image-6.png' alt=''>
+                            </div>
+                            <div class='item-description'>
+                                <h2>Lorem ipsum</h2>
+                                <p>$599.00</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="swiper-slide">
+                        <div class='viewed-products-item'>
+                            <div class='item-image'>
+                                <img src='<?php echo get_template_directory_uri() ?>/assets/images/catalog-image-1.png' alt=''>
+                            </div>
+                            <div class='item-description'>
+                                <h2>Lorem ipsum</h2>
+                                <p>$599.00</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="swiper-slide">
+                        <div class='viewed-products-item'>
+                            <div class='item-image'>
+                                <img src='<?php echo get_template_directory_uri() ?>/assets/images/catalog-image-2.png' alt=''>
+                            </div>
+                            <div class='item-description'>
+                                <h2>Lorem ipsum</h2>
+                                <p>$599.00</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- If we need navigation buttons -->
+            <div class="swiper-button-prev"></div>
+            <div class="swiper-button-next"></div>
+        </div>
+    </section>
 <?php get_footer(); ?>
