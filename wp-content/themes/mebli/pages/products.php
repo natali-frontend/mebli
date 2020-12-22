1d3a8bd2d5989ae9647009f72cc5ea75663aa202
Nataliya Rohochaya <lc4821577@gmail.com>
2020-12-22 18:49:57 +0200

    feature: products

--- wp-content/themes/mebli/pages/products.php
+++ wp-content/themes/mebli/pages/products.php
@@ -1,4 +1,94 @@
 <?php /* Template Name: Page - Products */ ?>
 <?php get_header(); ?>
-<section>Продукты</section>
+    <section class='feature'>
+        <div class='container'>
+            <div class='feature-description'>
+                <h2>Характеристика</h2>
+                <div class='description-list'>
+                    <div class='description-item'>
+                        <p>Характеристика</p>
+                        <p>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
+                        </p>
+                    </div>
+                    <div class='description-item'>
+                        <p>Характеристика</p>
+                        <p>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section class='previously'>
+        <h2>Ранее доставленные диваны</h2>
+        <div class="swiper-container swiper-previously">
+            <!-- Additional required wrapper -->
+            <div class="swiper-wrapper">
+                <!-- Slides -->
+                <div class="swiper-slide">
+                    <div class='previously-inner'>
+                        <div class='previously-item'>
+                            <div class='item-image'>
+                                <img src='<?php echo get_template_directory_uri() ?>/assets/images/previously-image-1.png' alt=''>
+                            </div>
+                            <div class='item-image'>
+                                <img src='<?php echo get_template_directory_uri() ?>/assets/images/previously-image-2.png' alt=''>
+                            </div>
+                        </div>
+                        <div class='previously-item'>
+                            <div class='item-image'>
+                                <img src='<?php echo get_template_directory_uri() ?>/assets/images/previously-image-3.png' alt=''>
+                            </div>
+                        </div>
+                        <div class='previously-item'>
+                            <div class='item-image'>
+                                <img src='<?php echo get_template_directory_uri() ?>/assets/images/previously-image-4.png' alt=''>
+                            </div>
+                        </div>
+                        <div class='previously-item'>
+                            <div class='item-image'>
+                                <img src='<?php echo get_template_directory_uri() ?>/assets/images/previously-image-5.png' alt=''>
+                            </div>
+                            <div class='item-image'>
+                                <img src='<?php echo get_template_directory_uri() ?>/assets/images/previously-image-6.png' alt=''>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="swiper-slide">
+                    <div class='previously-inner'>
+                        <div class='previously-item'>
+                            <div class='item-image'>
+                                <img src='<?php echo get_template_directory_uri() ?>/assets/images/previously-image-1.png' alt=''>
+                            </div>
+                            <div class='item-image'>
+                                <img src='<?php echo get_template_directory_uri() ?>/assets/images/previously-image-2.png' alt=''>
+                            </div>
+                        </div>
+                        <div class='previously-item'>
+                            <div class='item-image'>
+                                <img src='<?php echo get_template_directory_uri() ?>/assets/images/previously-image-3.png' alt=''>
+                            </div>
+                        </div>
+                        <div class='previously-item'>
+                            <div class='item-image'>
+                                <img src='<?php echo get_template_directory_uri() ?>/assets/images/previously-image-4.png' alt=''>
+                            </div>
+                        </div>
+                        <div class='previously-item'>
+                            <div class='item-image'>
+                                <img src='<?php echo get_template_directory_uri() ?>/assets/images/previously-image-5.png' alt=''>
+                            </div>
+                            <div class='item-image'>
+                                <img src='<?php echo get_template_directory_uri() ?>/assets/images/previously-image-6.png' alt=''>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <a href='#' class='btn'>Замовити</a>
+    </section>
 <?php get_footer(); ?>
