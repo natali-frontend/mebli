aa8a0723304b0e4f643a04761c6e4ec1d1dedc83
Nataliya Rohochaya <lc4821577@gmail.com>
2020-12-28 18:36:58 +0200

    feature: section gallery photography

--- wp-content/themes/mebli/pages/gallery.php
+++ wp-content/themes/mebli/pages/gallery.php
@@ -11,4 +11,25 @@
             </div>
         </div>
     </section>
+    <section class='gallery-photography'>
+        <div class='container'>
+            <div class='photography-inner'>
+                <div class='photography-list'>
+                    <div class='photography-item'><img src='<?php echo get_template_directory_uri() ?>/assets/images/photography-image-1.png' alt=''></div>
+                    <div class='photography-item'><img src='<?php echo get_template_directory_uri() ?>/assets/images/photography-image-2.png' alt=''></div>
+                    <div class='photography-item'><img src='<?php echo get_template_directory_uri() ?>/assets/images/photography-image-3.png' alt=''></div>
+                </div>
+                <div class='photography-list'>
+                    <div class='photography-item'><img src='<?php echo get_template_directory_uri() ?>/assets/images/photography-image-4.png' alt=''></div>
+                    <div class='photography-item'><img src='<?php echo get_template_directory_uri() ?>/assets/images/photography-image-5.png' alt=''></div>
+                </div>
+                <div class='photography-list'>
+                    <div class='photography-item'><img src='<?php echo get_template_directory_uri() ?>/assets/images/photography-image-6.png' alt=''></div>
+                    <div class='photography-item'><img src='<?php echo get_template_directory_uri() ?>/assets/images/photography-image-7.png' alt=''></div>
+                    <div class='photography-item'><img src='<?php echo get_template_directory_uri() ?>/assets/images/photography-image-8.png' alt=''></div>
+                    <div class='photography-item'><img src='<?php echo get_template_directory_uri() ?>/assets/images/photography-image-9.png' alt=''></div>
+                </div>
+            </div>
+        </div>
+    </section>
 <?php get_footer(); ?>
