6f4bf6b1a7feb016fa0eaeb988c65e89e510eeef
Mykola Melnyk <melnyk.cto.dev@gmail.com>
2021-02-02 03:52:10 +0200

    features: create template "viewed-products.php"

--- wp-content/themes/mebli/pages/home.php
+++ wp-content/themes/mebli/pages/home.php
@@ -151,57 +151,8 @@
             </div>
         </div>
     </section>
-<?php include get_template_directory() . '/template-part/previously-product.php'; ?>
-    <section class='viewed-products'>
-        <div class='container'>
-            <!-- Slider main container-->
-            <div class="swiper-container swiper-viewed">
-                <!--Additional required wrapper-->
-                <div class="swiper-wrapper">
-                    <!-- Slides -->
-                    <div class="swiper-slide">
-                        <div class='viewed-products-item'>
-                            <a href='/about-store' class='item-image'>
-                                <img src='<?php echo get_template_directory_uri() ?>/assets/images/catalog-image-6.png'
-                                     alt=''>
-                            </a>
-                            <div class='item-description'>
-                                <a href='/about-store'>Lorem ipsum</a>
-                                <p>$599.00</p>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="swiper-slide">
-                        <div class='viewed-products-item'>
-                            <a href='/about-store' class='item-image'>
-                                <img src='<?php echo get_template_directory_uri() ?>/assets/images/catalog-image-1.png'
-                                     alt=''>
-                            </a>
-                            <div class='item-description'>
-                                <a href='/about-store'>Lorem ipsum</a>
-                                <p>$599.00</p>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="swiper-slide">
-                        <div class='viewed-products-item'>
-                            <a href='/about-store' class='item-image'>
-                                <img src='<?php echo get_template_directory_uri() ?>/assets/images/catalog-image-2.png'
-                                     alt=''>
-                            </a>
-                            <div class='item-description'>
-                                <a href='/about-store'>Lorem ipsum</a>
-                                <p>$599.00</p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <!-- If we need navigation buttons -->
-            <div class="swiper-button-prev"></div>
-            <div class="swiper-button-next"></div>
-        </div>
-    </section>
+    <?php include get_template_directory() . '/template-part/previously-product.php'; ?>
+    <?php include get_template_directory() . '/template-part/viewed-products.php'; ?>
     <script>
         // Header
         const header = document.getElementById("header");
---
+++ wp-content/themes/mebli/template-part/viewed-products.php
@@ -0,0 +1,50 @@
+<section class='viewed-products'>
+    <div class='container'>
+        <!-- Slider main container-->
+        <div class="swiper-container swiper-viewed">
+            <!--Additional required wrapper-->
+            <div class="swiper-wrapper">
+                <!-- Slides -->
+                <div class="swiper-slide">
+                    <div class='viewed-products-item'>
+                        <a href='/about-store' class='item-image'>
+                            <img src='<?php echo get_template_directory_uri() ?>/assets/images/catalog-image-6.png'
+                                 alt=''>
+                        </a>
+                        <div class='item-description'>
+                            <a href='/about-store'>Lorem ipsum</a>
+                            <p>$599.00</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="swiper-slide">
+                    <div class='viewed-products-item'>
+                        <a href='/about-store' class='item-image'>
+                            <img src='<?php echo get_template_directory_uri() ?>/assets/images/catalog-image-1.png'
+                                 alt=''>
+                        </a>
+                        <div class='item-description'>
+                            <a href='/about-store'>Lorem ipsum</a>
+                            <p>$599.00</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="swiper-slide">
+                    <div class='viewed-products-item'>
+                        <a href='/about-store' class='item-image'>
+                            <img src='<?php echo get_template_directory_uri() ?>/assets/images/catalog-image-2.png'
+                                 alt=''>
+                        </a>
+                        <div class='item-description'>
+                            <a href='/about-store'>Lorem ipsum</a>
+                            <p>$599.00</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- If we need navigation buttons -->
+        <div class="swiper-button-prev"></div>
+        <div class="swiper-button-next"></div>
+    </div>
+</section>
