e649af4da5f119cd06bf5fc041e626f2094a44aa
Nataliya Rohochaya <lc4821577@gmail.com>
2021-02-25 18:54:13 +0200

    fix: replace src for tag img

--- wp-content/themes/mebli/template-part/viewed-products.php
+++ wp-content/themes/mebli/template-part/viewed-products.php
@@ -8,36 +8,36 @@
                 <div class="swiper-slide">
                     <div class='viewed-products-item'>
                         <a href='/about-store' class='item-image'>
-                            <img src='<?php echo get_template_directory_uri() ?>/assets/images/catalog-image-6.png'
+                            <img src='<?php echo get_template_directory_uri() ?>/assets/images/baron-1.jpg'
                                  alt=''>
                         </a>
                         <div class='item-description'>
-                            <a href='/about-store'>Lorem ipsum</a>
-                            <p>$599.00</p>
+                            <a href='/about-store'>Барон Мини</a>
+                            <p>40 352 ₴</p>
                         </div>
                     </div>
                 </div>
                 <div class="swiper-slide">
                     <div class='viewed-products-item'>
                         <a href='/about-store' class='item-image'>
-                            <img src='<?php echo get_template_directory_uri() ?>/assets/images/catalog-image-1.png'
+                            <img src='<?php echo get_template_directory_uri() ?>/assets/images/lord-1.jpg'
                                  alt=''>
                         </a>
                         <div class='item-description'>
-                            <a href='/about-store'>Lorem ipsum</a>
-                            <p>$599.00</p>
+                            <a href='/about-store'>Угловой Лорд</a>
+                            <p>28 260 ₴</p>
                         </div>
                     </div>
                 </div>
                 <div class="swiper-slide">
                     <div class='viewed-products-item'>
                         <a href='/about-store' class='item-image'>
-                            <img src='<?php echo get_template_directory_uri() ?>/assets/images/catalog-image-2.png'
+                            <img src='<?php echo get_template_directory_uri() ?>/assets/images/domino-1.jpg'
                                  alt=''>
                         </a>
                         <div class='item-description'>
-                            <a href='/about-store'>Lorem ipsum</a>
-                            <p>$599.00</p>
+                            <a href='/about-store'>Трансформер Домино</a>
+                            <p>38 276 ₴</p>
                         </div>
                     </div>
                 </div>
