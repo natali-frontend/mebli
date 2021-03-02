4a8b24ce9a09b456d1f3a2c077732618b0146bbe
Mykola Melnyk <melnyk.cto.dev@gmail.com>
2021-03-02 10:00:26 +0200

    features: dynamic catalog

--- wp-content/themes/mebli/pages/catalog.php
+++ wp-content/themes/mebli/pages/catalog.php
@@ -3,102 +3,34 @@
     <section class='catalog-inner'>
         <div class='container'>
             <ul class='inner-list'>
-                <li class='inner-item'>
-                    <a href='/products' class='item-image'>
-                        <div class='loading'>
-                            <img src='<?php echo get_template_directory_uri() ?>/assets/images/icons/loading.svg' alt=''>
-                        </div>
-                        <img class='main-image' src='<?php echo get_template_directory_uri() ?>/assets/images/baron-1.jpg' alt=''>
-                    </a>
-                    <div class='item-description'>
-                        <h2>Барон Мини</h2>
-                        <p>40 352 ₴</p>
-                    </div>
-                </li>
-                <li class='inner-item'>
-                    <a href='/products' class='item-image'>
-                        <div class='loading'>
-                            <img src='<?php echo get_template_directory_uri() ?>/assets/images/icons/loading.svg' alt=''>
-                        </div>
-                        <img class='main-image' src='<?php echo get_template_directory_uri() ?>/assets/images/armchair-1.jpg' alt=''>
-                    </a>
-                    <div class='item-description'>
-                        <h2>Кресло Лорд</h2>
-                        <p>21 212 ₴</p>
-                    </div>
-                </li>
-                <li class='inner-item'>
-                    <a href='/products' class='item-image'>
-                        <div class='loading'>
-                            <img src='<?php echo get_template_directory_uri() ?>/assets/images/icons/loading.svg' alt=''>
-                        </div>
-                        <img class='main-image' src='<?php echo get_template_directory_uri() ?>/assets/images/domino-1.jpg' alt=''>
-                    </a>
-                    <div class='item-description'>
-                        <h2>Трансформер Домино</h2>
-                        <p>38 276 ₴</p>
-                    </div>
-                </li>
-                <li class='inner-item'>
-                    <a href='/products' class='item-image'>
-                        <div class='loading'>
-                            <img src='<?php echo get_template_directory_uri() ?>/assets/images/icons/loading.svg' alt=''>
-                        </div>
-                        <img class='main-image' src='<?php echo get_template_directory_uri() ?>/assets/images/lord-1.jpg' alt=''>
-                    </a>
-                    <div class='item-description'>
-                        <h2>Угловой Лорд</h2>
-                        <p>28 260 ₴</p>
-                    </div>
-                </li>
-<!--                <li class='inner-item'>-->
-<!--                    <a href='/products' class='item-image'>-->
-<!--                        <div class='loading'>-->
-<!--                            <img src='--><?php //echo get_template_directory_uri() ?><!--/assets/images/icons/loading.svg' alt=''>-->
-<!--                        </div>-->
-<!--                        <img class='main-image' src='--><?php //echo get_template_directory_uri() ?><!--/assets/images/catalog-image-5.png' alt=''>-->
-<!--                    </a>-->
-<!--                    <div class='item-description'>-->
-<!--                        <h2>Lorem ipsum</h2>-->
-<!--                        <p>$599.00</p>-->
-<!--                    </div>-->
-<!--                </li>-->
-<!--                <li class='inner-item'>-->
-<!--                    <a href='/products' class='item-image'>-->
-<!--                        <div class='loading'>-->
-<!--                            <img src='--><?php //echo get_template_directory_uri() ?><!--/assets/images/icons/loading.svg' alt=''>-->
-<!--                        </div>-->
-<!--                        <img class='main-image' src='--><?php //echo get_template_directory_uri() ?><!--/assets/images/catalog-image-6.png' alt=''>-->
-<!--                    </a>-->
-<!--                    <div class='item-description'>-->
-<!--                        <h2>Lorem ipsum</h2>-->
-<!--                        <p>$599.00</p>-->
-<!--                    </div>-->
-<!--                </li>-->
-<!--                <li class='inner-item'>-->
-<!--                    <a href='/products' class='item-image'>-->
-<!--                        <div class='loading'>-->
-<!--                            <img src='--><?php //echo get_template_directory_uri() ?><!--/assets/images/icons/loading.svg' alt=''>-->
-<!--                        </div>-->
-<!--                        <img class='main-image' src='--><?php //echo get_template_directory_uri() ?><!--/assets/images/catalog-image-7.png' alt=''>-->
-<!--                    </a>-->
-<!--                    <div class='item-description'>-->
-<!--                        <h2>Lorem ipsum</h2>-->
-<!--                        <p>$599.00</p>-->
-<!--                    </div>-->
-<!--                </li>-->
-<!--                <li class='inner-item'>-->
-<!--                    <a href='/products' class='item-image'>-->
-<!--                        <div class='loading'>-->
-<!--                            <img src='--><?php //echo get_template_directory_uri() ?><!--/assets/images/icons/loading.svg' alt=''>-->
-<!--                        </div>-->
-<!--                        <img class='main-image' src='--><?php //echo get_template_directory_uri() ?><!--/assets/images/catalog-image-8.png' alt=''>-->
-<!--                    </a>-->
-<!--                    <div class='item-description'>-->
-<!--                        <h2>Lorem ipsum</h2>-->
-<!--                        <p>$599.00</p>-->
-<!--                    </div>-->
-<!--                </li>-->
+                <?php
+                    $current_page = (get_query_var('paged')) ? get_query_var('paged') : 1;
+                    $params = array(
+                        'posts_per_page' => 5,
+                        'post_type' => 'post',
+                        'paged' => $current_page
+                    );
+                    query_posts($params);
+                    while (have_posts()) : the_post(); ?>
+                        <li class='inner-item'>
+                            <a href='<?php echo get_permalink(); ?>' class='item-image'>
+                                <div class='loading'>
+                                    <img src='<?php echo get_template_directory_uri() ?>/assets/images/icons/loading.svg'
+                                         alt=''>
+                                </div>
+                                <img class='main-image'
+                                     src='<?php echo get_template_directory_uri() ?>/assets/images/armchair-1.jpg'
+                                     alt=''>
+                            </a>
+                            <div class='item-description'>
+                                <?php
+                                    $field = get_field_object('product_price');
+                                ?> <h2><?php echo get_the_title(); ?></h2>
+                                <p><?php echo $field['value']; ?></p>
+                            </div>
+                        </li>
+                    <?php endwhile; ?>
+                <?php wp_reset_postdata(); ?>
             </ul>
         </div>
     </section>
